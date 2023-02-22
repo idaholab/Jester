cdb2783ec1ac120a45834937a98f5cfbc5bd141d
john-darrington <john.darrington@inl.gov>
2023-02-22 10:17:03 -0700

    added new error type

--- jester_core/src/errors.rs
+++ jester_core/src/errors.rs
@@ -1,7 +1,11 @@
+use std::any::Any;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ProcessorError {
     #[error("unknown processor error")]
     Unknown,
+
+    #[error("thread error")]
+    ThreadError(Box<dyn Any + Send>),
 }
